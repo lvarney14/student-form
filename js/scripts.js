ae0e73c85c8c4c37f6178df19b6a808f9ee25026
Lauren Varney <l_varney_14@hotmail.com>
2014-02-19 22:31:49 -0800

    1st commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	$('h1').lettering();
+
+	$('h3').lettering('words');
+
+	$('h3 + p').lettering('lines')
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
---
+++ validation.js
@@ -0,0 +1,18 @@
+/*global jQuery */
+/*!	
+* Lettering.JS 0.6.1
+*
+* Copyright 2010, Dave Rupert http://daverupert.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+*
+* Thanks to Paul Irish - http://paulirish.com - for the feedback.
+*
+* Date: Mon Sep 20 17:14:00 2010 -0600
+*/
+(function($){
+	function injector(t, splitter, klass, after) {
+		var a = t.text().split(splitter), inject = '';
+		if(isset($_POST['email'])) {
+     
+ 
